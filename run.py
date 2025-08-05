93604d996c76758f6a856f82d5f1eaae42a27c09
Nakul <aiclass584@gmail.com>
2025-08-05 17:31:39 +0530

    First commit

---
+++ run.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__": 
+    a=int(input("first no."))
+    b=int(input("second no."))
+    c=int(input("third no."))
+
+    sum=a+b+c
+    avg=(a+b+c)/3
+    mul=a*b*c
+
+
+    print("sum",sum,"avarge",avg,"product",mul)
+if avg >50:
+    print("avarage is greater than 50")
+else:
+    ("no")
+   
